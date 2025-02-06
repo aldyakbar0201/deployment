55fa2f247595f39d5763dc336f2adebc6b4d241e
aldyakbar0201 <aldyhidayatulakbar0201@gmail.com>
2025-02-06 12:14:56 +0700

    remove trash

--- apps/server/dist/src/app.js
+++ apps/server/dist/src/app.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv/Config");
+require("dotenv/config");
 const express_1 = __importDefault(require("express"));
 const prisma_1 = __importDefault(require("./configs/prisma"));
 const app = (0, express_1.default)();
